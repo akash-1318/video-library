9b5b8632ee43223f84a08fff8722f72ccf44aef5
akash18baaz <akashsharma@Akashs-MacBook-Air.local>
2022-07-03 14:41:17 +0530

    fix - fixed some bugs and polished features

--- src/components/videos-section/videos-section.js
+++ src/components/videos-section/videos-section.js
@@ -1,61 +1,91 @@
 import "./videos-section.css";
 import { VideoCard } from "../compIndex";
 import { useVideoContext } from "../../contexts/video-context";
-import {usePrimaryStatesContext} from "../../contexts/primary-states-context"
+import { usePrimaryStatesContext } from "../../contexts/primary-states-context";
 import { filteredSearchedVideos } from "../../utils/filter-videos";
-import {useThemeContext} from "../../contexts/theme-context"
+import { useThemeContext } from "../../contexts/theme-context";
+import empty from "../../assets/images/Empty-bag.svg";
+import { useState } from "react";
 
 function VideosSection() {
   const { state, dispatch } = useVideoContext();
-  const {searchInput, setSearchInput} = usePrimaryStatesContext();
-  const {theme} = useThemeContext();
+  const { searchInput, setSearchInput } = usePrimaryStatesContext();
+  const { theme } = useThemeContext();
   const { videosData, categoriesData, singleCategory } = state;
+  const [activeState, setActiveState] = useState(null);
 
-  console.log(searchInput)
-
-  const getFilteredSearchedVideos = filteredSearchedVideos(videosData, singleCategory, searchInput);
+  const getFilteredSearchedVideos = filteredSearchedVideos(
+    videosData,
+    singleCategory,
+    searchInput
+  );
 
   const filterActive = (category) => {
-      if(category === singleCategory){
-          return "active"
-      }
-  }
-  
+    if (category === singleCategory) {
+      return "active";
+    }
+  };
+
   return (
     <section className="main__video-container">
       <div className="header__nav-middle inside__video-container">
         <div className={`search__icon ${theme === "light" ? "" : "dark"}`}>
           <i className="bx bx-search-alt-2"></i>
         </div>
-        <input placeholder="search" className="header__search"
-        onChange = {(e) => setSearchInput(e.target.value)} 
+        <input
+          placeholder="search"
+          className="header__search"
+          onChange={(e) => setSearchInput(e.target.value)}
         />
       </div>
-      <div className="category__container">
-        <button
-          className={`category__item ${filterActive("All")} ${theme === "light" ? "" : "dark"} `}
-          onClick={() => dispatch({ type: "SET_CATEGORY", payload: "All" })}
-        >
-          All
-        </button>
-        {categoriesData.map((item) => {
-          return (
+      {getFilteredSearchedVideos.length === 0 ? (
+        <>
+          <div className="empty__video-container">
+            <h2>No Video found!</h2>
+            <img src={empty} className="empty__img" />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="category__container">
             <button
-              className={`category__item ${filterActive(item.categoryName)} ${theme === "light" ? "" : "dark"}`}
-              onClick={() =>
-                dispatch({ type: "SET_CATEGORY", payload: item.categoryName })
-              }
+              className={`category__item ${filterActive("All")} ${
+                theme === "light" ? "" : "dark"
+              } `}
+              onClick={() => dispatch({ type: "SET_CATEGORY", payload: "All" })}
             >
-              {item.categoryName}
+              All
             </button>
-          );
-        })}
-      </div>
-      <div className="videos__container">
-        {getFilteredSearchedVideos.map((video) => {
-          return <VideoCard videoData={video} key = {video._id} />;
-        })}
-      </div>
+            {categoriesData.map((item) => {
+              return (
+                <button
+                  className={`category__item ${filterActive(
+                    item.categoryName
+                  )} ${theme === "light" ? "" : "dark"}`}
+                  onClick={() =>
+                    dispatch({
+                      type: "SET_CATEGORY",
+                      payload: item.categoryName,
+                    })
+                  }
+                >
+                  {item.categoryName}
+                </button>
+              );
+            })}
+          </div>
+          <div className="videos__container">
+            {getFilteredSearchedVideos.map((video, index) => {
+              return <VideoCard 
+              videoData={video} 
+              key={video._id}
+              activeState = {activeState}
+              setActiveState = {setActiveState}
+              />;
+            })}
+          </div>
+        </>
+      )}
     </section>
   );
 }
